17626ea743ead9c6c2cd1d6e3393165031233f57
Reginawambui <ngendoanita8@gmail.com>
2023-10-07 20:24:39 +0300

    functions

---
+++ index.js
@@ -0,0 +1,19 @@
+let divide = function (num1, num2){
+    console.log(num1/num2)
+}
+
+divide(2000,100)
+
+let square = (num3) => {
+    console.log(num3 ** 2)
+}
+square(num3)
+ 
+let add = (num4, num5) => {
+    num4 + num5
+
+}
+add(num4,num5)
+
+    
+
